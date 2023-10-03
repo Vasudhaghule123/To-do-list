54dc3ecc4e6f9b3e040c85ad1fe60872d769af60
Vasudha Ghule <146065753+Vasudhaghule123@users.noreply.github.com>
2023-10-04 03:05:20 +0530

    file added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import TodoList from './components/TodoList';
+
+function App() {
+  return (
+    <div className="to-do-app">
+      
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
